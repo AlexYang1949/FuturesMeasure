64a53f66de923cc2863352e79cd2c6b7201067ca
杨照珩 <yangzhaoheng@yangzhaohengdeMacBook-Pro.local>
2017-06-14 18:01:31 +0800

    add time

--- charge/chargeManager.py
+++ charge/chargeManager.py
@@ -47,7 +47,8 @@ class ChargeManager():
 
         self.chargeResult.all_assets += dis
         self.chargeResult.asset_array.append((date, self.chargeResult.all_assets))
-        charge_string  = '日期 : %s  幅度 : %.2f  价格 ：%.2f' %(str(date),precent,price)
+        direction = '涨' if self.chargeModel.hold_direct == 1 else '跌'
+        charge_string  = '日期 : %s  幅度 : %.2f  价格 ：%.2f 持有时间 %d 方向:%s' %(str(date),precent,price,self.chargeModel.hold_days,direction)
         if dis > 0:
             self.chargeResult.get_time += 1
             self.chargeResult.total_get += precent
@@ -76,7 +77,6 @@ class ChargeManager():
                 self.chargeResult.big_array.append(charge_string)
 
         if self.nodeStat:
-            direction = '涨' if self.chargeModel.hold_direct==1 else '跌'
             print  '震荡 %d 时间 %s 价格：%.f 幅度:%.2f 持有时间 %d 方向:%s' % (self.chargeResult.gap_lost_time,str(date), price,precent,self.chargeModel.hold_days,direction)
         self.chargeModel.hold_days = 0
 
